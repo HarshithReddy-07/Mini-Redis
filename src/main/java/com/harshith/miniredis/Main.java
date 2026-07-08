5df49b0773a7fcb394abfdc6a63c4fc820074f8f
HarshithReddy-07 <harshithreddybathula@gmail.com>
2026-07-08 18:25:57 +0530

    commited main.java

--- src/main/java/com/harshith/miniredis/Main.java
+++ src/main/java/com/harshith/miniredis/Main.java
@@ -1,7 +1,13 @@
 package com.harshith.miniredis;
 
+import com.harshith.miniredis.server.MiniRedisServer;
+import com.harshith.miniredis.storage.StorageEngine;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         
+        StorageEngine engine = new StorageEngine("data/database.wal");
+        MiniRedisServer server = new MiniRedisServer(6379, engine);
+        server.start();
     }
 }
